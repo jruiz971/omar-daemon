6af555b1110860af6f11179ef314af4bdbeb6a94
jruiz971 <juanluisruiz971@gmail.com>
2023-01-27 08:29:36 -0600

    Added modules to save data

--- main.py
+++ main.py
@@ -2,12 +2,40 @@
 
     2023 Juan Luis Ruiz Vanegas (juanluisruiz971@gmail.com)
 """
+from datetime import datetime
 
 from Acquisition import Acquisition
+from utils.db import DatabaseScripts
+
+
+def get_data(acquisition):
+    acquisition.read_config()
+    acquisition.read_samples()
+    acquisition.save_samples()
+
+    execution_time = datetime.now()
+    return execution_time
+
+
+def get_data_same_config(acquisition):
+    acquisition.read_samples()
+    acquisition.save_samples()
+
+    execution_time = datetime.now()
+    return execution_time
+
 
 if __name__ == '__main__':
     acquisition = Acquisition()
 
-    acquisition.read_config()
-    acquisition.read_samples()
-    acquisition.save_samples()
+    execution_time = get_data(acquisition)
+
+    if execution_time > datetime.now() + datetime.timedelta(minutes=5):
+        execution_time = get_data(acquisition)
+    else:
+        execution_time = get_data_same_config(acquisition)
+
+    db_manager = DatabaseScripts()
+    db_manager.save_file_to_server(acquisition.FILEPATH)
+
+    check_semaforo
